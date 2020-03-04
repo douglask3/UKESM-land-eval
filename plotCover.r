690bac0e614242cd9c713c8419167fce565dfa43
Douglas Kelley <douglas.i.kelley@gmail.com>
2020-03-04 14:13:57 +0000

    scores for each regions

--- plotCover.r
+++ plotCover.r
@@ -1,7 +1,9 @@
+library(raster)
 source("libs/plotStandardMap.r")
 source("libs/addLetLab.r")
 source("libs/sd.raster.r")
 library("plotrix")
+source("libs/loadRegionInfo.r")
 library(maps)
 
 library(raster)
@@ -12,7 +14,7 @@ graphics.off()
 
 obs_files = c(IGBP = "igbp", CCI = "cci", VCF = "VCF")
 
-jobs = c("u-az513", "u-az515", "u-az524", "u-bb277", "u-bc179", "u-bc292", "u-bc370", "u-bc470", "u-bd288", "u-bd416", "u-bd483")
+jobs = c("u-az513", "u-az515", "u-az524", "u-bb277", "u-bc179", "u-bc292", "u-bc370", "u-bc470", "u-bd288", "u-bd416", "u-bd483")[1:3]
 
 vars = c("tree", "wood", "shrub", "grass")
 
@@ -22,15 +24,27 @@ cols = c('#ffffe5','#f7fcb9','#d9f0a3','#addd8e','#78c679','#41ab5d','#238443','
 files = list.files("outputs/", full.names = TRUE)
 
 
-MM <- function(a, b) {
+MM <- function(a, b, byRegions = TRUE) {
+    print("running MM")
     if (nlayers(a) == 1) {  
         a = addLayer(a, 1-a)
         b = addLayer(b, 1-b) 
     }
+    
     ar = raster::area(a)
+    ar[is.na(regions)] = NaN
     ar[is.na(a[[1]])] = NaN
-    score = 2*sum.raster(abs(a-b) * ar, na.rm = TRUE)/ sum.raster(ar, na.rm = T)
-    return(score)
+    MMi <- function(region) {
+        if (!is.na(region)) ar[regions!=region] = 0
+        score = 2*sum.raster(abs(a-b) * ar, na.rm = TRUE)/ sum.raster(ar, na.rm = T)    
+        return(score)
+    }
+    if (byRegions) {
+        scores = sapply(c(NaN, 1:28), MMi)
+        names(scores) = c("Global", region_names)
+    }
+    else scores = MMi(NaN)
+    return(scores)
 }
 
 median.raster <- function(...) 
@@ -61,8 +75,37 @@ compRR <- function(r) {
         return(score)
     }
     scores = sapply(1:10, Mod)
-    return(c(mean(scores), sd(scores)))
+    return(cbind(apply(scores, 1, mean), apply(scores, 1, sd)))
 }
+
+areaWmean <- function(r) {
+    ar = raster::area(r)
+    ar[is.na(r)] = 0.0
+    ar[is.na(regions)] = 0.0
+    r[is.na(regions)] = NaN
+
+    fRegion <- function(region, std = FALSE) {
+        if (!is.na(region)) {
+            test = regions != region
+            r[test] = NaN
+            ar[test] = 0.0
+        }
+        if (std) out = FUN.raster(sd, r, na.rm = TRUE)
+        else out = sum.raster(r * ar, na.rm = TRUE)/sum.raster(ar, na.rm = TRUE)
+        return(out)
+    }
+    fRegions <- function(...) {
+        out = sapply(c(NaN, 1:28), fRegion, ...)
+        names(out) = c("Global", region_names)
+        return(out)
+    }
+    mns = fRegions()
+    sds = fRegions(std = TRUE)  
+    out = cbind(mns, sds)
+    colnames(out) = c("cover mean", "cover sd")
+    return(out)   
+}
+
 
 plotVariable <- function(var) {
      files = files[grepl(var, files)]
@@ -106,30 +149,55 @@ plotVariable <- function(var) {
     
     sims_mean =  mean(layer.apply(sims, function(i) i))
     sims_sd =  sd.raster( (layer.apply(sims, function(i) i)))
-    plotStandardMap(sims_mean*100, e = sims_sd, cols = cols, limits = limits,  ePatternRes = 22, ePatternThick = 0.38, limits_error = c(0.01, 0.05))
+    plotStandardMap(sims_mean*100, e = sims_sd, cols = cols, limits = limits,
+                    ePatternRes = 22, ePatternThick = 0.38, limits_error = c(0.01, 0.05))
     if (var == vars[[1]]) mtext(side = 3, 'UKESM')
     
-    tab = sapply(sims, function(sim) sapply(obss, MM, sim))
-    tabi = cbind(apply(tab, 1, mean), apply(tab, 1, sd))
+    benchmarkObs <- function(obs, name) {
+        tab = lapply(sims, areaWmean)
+        tab = do.call(cbind, tab)
+        colnames(tab) = paste(rep(jobs, each = 2), '-', colnames(tab))
+        tab_mns = cbind(areaWmean(obs), tab)
+        colnames(tab_mns)[1:2] = c("Obs - cover mean", "Obs - sd")
+
+        tab_ben = sapply(sims, MM, obs)
+        colnames(tab_ben) = paste(jobs, '- MM')
+        
+        tab_null = cbind(compmn(obs, median.raster), compmn(obs), compRR(obs))       
+        colnames(tab_null) = c('median', 'mean', 'randomly-resampled mean',
+                          'randomly-resampled sd')
+
+        tabi <- function(tab) cbind(apply(tab, 1, mean), apply(tab, 1, sd))
+        
+        tab_ens = cbind(tab_mns[,1],
+                        tabi(tab_mns[,seq(3, ncol(tab_mns)-1, by = 2)]),
+                        tabi(tab_ben))
+        colnames(tab_ens) = c("Obs - cover", 
+                              "UKESM - cover mean", "UKESM - cover sd",
+                              "UKESM - MM mean", "UKESM - MM sd")
+        
+        tab = cbind(tab_mns, tab_ben, tab_null)
+        fname = paste0("docs/", var, '-', name, "-MM-full.csv")
+        tab = round(tab, 3)
+        write.csv(tab, file = fname)
+
+        tab_ens = cbind(tab_ens, tab_null)
+        fname = paste0("docs/", var, '-', name, "-MM-summ.csv")
+        tab_ens = round(tab_ens, 3)
+        write.csv(tab_ens, file = fname)
+        
+    }
+    bench = mapply(benchmarkObs, obss, names(obss))
+    #name = paste0("docs/", var, "-MM-full.csv")
+    #tab = round(tab, 3)
+    #write.csv(tab, file = fname)
     
-    tab = cbind(tab, sapply(obss, compmn, median.raster))
-    tab = cbind(tab, sapply(obss, compmn))
+    #colnames(tabi) = c("UKESM-mean", "UKESM-sd")
+    #tabi = cbind(tabi, tab[, (ncol(tab)-3):ncol(tab)])
     
-    rrs = sapply(obss, compRR)
-    
-    tab = cbind(tab, t(rrs))
-    colnames(tab) = c(jobs, 'median', 'mean', 'randomly-resampled mean', 'randomly-resampled sd')
-    
-    fname = paste0("docs/", var, "-MM-full.csv")
-    tab = round(tab, 3)
-    write.csv(tab, file = fname)
-    
-    colnames(tabi) = c("UKESM-mean", "UKESM-sd")
-    tabi = cbind(tabi, tab[, (ncol(tab)-3):ncol(tab)])
-    
-    fname = paste0("docs/", var, "-MM-summ.csv")
-    tabi = round(tabi, 3)
-    write.csv(tabi, file = fname)
+    #fname = paste0("docs/", var, "-MM-summ.csv")
+    #tabi = round(tabi, 3)
+    #write.csv(tabi, file = fname)
     return(list(obss, sims))
 }
 
@@ -137,7 +205,8 @@ png("figs/vegDist.png", height = 183/2, width = 183, units = 'mm', res = 450)
     layout(rbind(t(matrix(1:16, nrow = 4, ncol = 4)), 17), heights = c(1, 1, 1, 1, 0.3))
     par( mar = rep(0,4), oma = rep(1.5, 4))
     out = lapply(vars, plotVariable)
-    StandardLegend(cols, limits, out[[1]][[1]][[1]], extend_max = FALSE, maxLab = 100, add = FALSE)
+    StandardLegend(cols, limits, out[[1]][[1]][[1]], extend_max = FALSE,
+                   maxLab = 100, add = FALSE)
 dev.off()
 itemComparison <- function(obs_name) {
     if(any(names(out[[1]][[1]])==obs_name)) index = c(1, 3, 4) else index = c(2, 4)
@@ -154,19 +223,22 @@ itemComparison <- function(obs_name) {
     modsi = lapply(1:length(out[[1]][[2]]), makeItemsMod)
     
     scores = sapply(modsi, MM, obsi)
-    scores = c(scores, mean(scores), sd(scores),
+    
+    scores = cbind(scores, mean(scores), sd(scores),
                compmn(obsi, median.raster), compmn(obsi), compRR(obsi))
-    names(scores) = c(jobs, 'UKESM-mean', 'UKESM-sd',
+    colnames(scores) = c(jobs, 'UKESM-mean', 'UKESM-sd',
                       'median', 'mean', 'randomly-resampled mean', 'randomly-resampled sd')
+
+    tab = round(t(scores), 3)
+
+    tabFull = tab[,c(1:(ncol(tab)-6), (ncol(tab)-3):ncol(tab))]
+    write.csv(tabFull, file = paste0("docs/items-", obs_name, "-MM-full.csv"))
+
+    tabSumm = tab[,(ncol(tab)-5):ncol(tab)]
+    write.csv(tabSumm, file = paste0("docs/items-", obs_name, "-MM-summ.csv"))
     return(scores)
 }
 
 items_scores = sapply(names(obs_files), itemComparison)
-tab = round(t(items_scores), 3)
 
-tabFull = tab[,c(1:(ncol(tab)-6), (ncol(tab)-3):ncol(tab))]
-write.csv(tabFull, file = "docs/items-MM-full.csv")
-
-tabSumm = tab[,(ncol(tab)-5):ncol(tab)]
-write.csv(tabSumm, file = "docs/items-MM-summ.csv")
 
