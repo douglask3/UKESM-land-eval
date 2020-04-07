5ee0411b10a0ecf0d463e97da9f7a2d6cf4b4675
Douglas Kelley <douglas.i.kelley@gmail.com>
2020-04-07 12:46:09 +0100

    adsapting for use here

--- libs/plotConPhase.r
+++ libs/plotConPhase.r
@@ -3,7 +3,8 @@ plotConPhase <- function(r, let = c('', ''), addLegend = FALSE,
                          phase_cols = c('#313695', '#a50026', '#ffff00','#313695'),
                          conc_cols = c('#ffffd9','#edf8b1','#c7e9b4','#7fcdbb','#41b6c4',
                                        '#1d91c0','#225ea8','#253494','#081d58'),
-                         phase_lims = 0.5:11.5,   conc_lims = seq(0, 1, 0.1)) {
+                         phase_lims = 0.5:11.5,   conc_lims = seq(0, 1, 0.1),
+                         regions = NULL) {
 
     pc = PolarConcentrationAndPhase.RasterBrick(r, phase_units = "months")
     plotStandardMap(pc[[1]], limits = phase_lims, cols = phase_cols)
@@ -14,4 +15,8 @@ plotConPhase <- function(r, let = c('', ''), addLegend = FALSE,
     if (addLegend)
         StandardLegend(limits = conc_lims, cols = conc_cols, extend_max = FALSE,
                         maxLab = 1, dat = pc[[2]], add = TRUE, oneSideLabels = FALSE) 
+
+    if (!is.null(regions)) {
+        browser()
+    }
 }
