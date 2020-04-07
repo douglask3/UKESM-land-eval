a3c8ace1cc286f0e54d0f817f7f5db09da9a8cd2
Douglas Kelley <douglas.i.kelley@gmail.com>
2020-04-07 19:11:43 +0100

    adding in phase and concentration

--- make_model_variables/clim.r
+++ make_model_variables/clim.r
@@ -13,16 +13,20 @@ run <- function(name, id, job) {
         dat = brick(file)
         dat = getYrsFromLayers(dat, year, NULL)
         clim = convert2Climatology(dat)   
-
+        pc = PolarConcentrationAndPhase(clim, phase_units = "months")
         dat = getYrsFromLayers(dat, year)
         MAD = mean(dat)
+    
         fname_out = paste0(out_dir, '/', name , '/')
         makeDir(fname_out)
-        fname_out = paste0(fname_out, c('mean_annual', 'climatology', 'annual'),
+        fname_out = paste0(fname_out, c('mean_annual', 'climatology',
+                                        'phase', 'concentration', 'annual'),
                            '-', min(year), '_', max(year), '-', job, '.nc')
-        writeRaster.Standard(MAD , fname_out[1])
-        writeRaster.Standard(clim, fname_out[2])
-        writeRaster.Standard(dat , fname_out[3])
+        writeRaster.Standard(MAD    , fname_out[1])
+        writeRaster.Standard(clim   , fname_out[2])
+        writeRaster.Standard(pc[[1]], fname_out[3])
+        writeRaster.Standard(pc[[2]], fname_out[4])
+        writeRaster.Standard(dat    , fname_out[5])
     }
     lapply(years, run4years)
 }
