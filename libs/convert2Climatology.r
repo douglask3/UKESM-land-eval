b06b029e01a8036afd2dc21403bd21b1de50d703
Douglas Kelley <douglas.i.kelley@gmail.com>
2020-04-07 14:50:11 +0100

    adding month names

--- libs/convert2Climatology.r
+++ libs/convert2Climatology.r
@@ -1,2 +1,7 @@
-convert2Climatology <- function(r) 
-    layer.apply(1:12, function(mn) mean(r[[seq(mn, nlayers(r), by = 12)]]))
+convert2Climatology <- function(r) {
+    nms = sapply(names(r), function(i) strsplit(i, '.', fixed = TRUE)[[1]][2])[1:12]
+    nms = paste0('X', nms)
+    out = layer.apply(1:12, function(mn) mean(r[[seq(mn, nlayers(r), by = 12)]]))
+    names(out) = nms
+    return(out)
+}
