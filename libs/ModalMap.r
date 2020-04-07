95743e703504e5ee7bd005b5d07cda67d97549c7
Douglas Kelley <douglas.i.kelley@gmail.com>
2020-04-07 12:44:51 +0100

    adapting for use

--- libs/ModalMap.r
+++ libs/ModalMap.r
@@ -2,7 +2,8 @@ modal_cols = c('#fff7f3','#fde0dd','#fcc5c0','#fa9fb5','#f768a1','#dd3497','#ae0
 modal_limits = c(1, 1.1, 1.2, 1.5, 2)
 
 ModalMap <- function(obs, txt, addLegend, let = NULL,
-                     cols = modal_cols, limits = modal_limits) {
+                     cols = modal_cols, limits = modal_limits, 
+                     regions = NULL) {
     modal = testModal(obs)
     modal_approx = layer.apply(2:6, function(i)
                               modal[[i]] * (0.5-cos(2*pi * modal[[i+6]]/12)/2))
@@ -15,5 +16,9 @@ ModalMap <- function(obs, txt, addLegend, let = NULL,
         StandardLegend(limits = limits - 1, cols = cols, dat = modal_approx-1,
                        extend_max = TRUE,
                        labelss = modal_limits, add = TRUE) 
+
+    if (!is.null(regions)) {
+        browser()
+    }
     return(modal)
 }
