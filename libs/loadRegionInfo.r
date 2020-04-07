5c20a134c749bfcb3d891422275ec2dd04b5f7e6
Douglas Kelley <douglas.i.kelley@gmail.com>
2020-04-07 12:45:29 +0100

    adding infor about region veg type

--- libs/loadRegionInfo.r
+++ libs/loadRegionInfo.r
@@ -29,4 +29,43 @@ region_names =  c("Australasia\nTropical Forest",
                  "Eurasian\nBoreal forest",
                  "Eurasian\nTundra")
 
+
+
+vegTypes = c("Tropical evergreen forest/wood",  #1
+             "Seasonal/temperate forest/wood",  #2 
+             "Savanna/Parkland/grassland",      #3
+             "Mediterranean",                   #4       
+             "Desert/shrubland",                #5
+             "Boreal Forest",                   #6
+             "Tundra")                          #7
+
+region_vegTypes = c("Australasia\nTropical Forest" = 1,        
+                    "South East\nAustralia Woodland" = 2,
+                    "Australia\nSavanna/grassland" = 3,
+                    "Australia\nMediterranean" = 4,
+                    "Australia\nDesert/shrubland" = 5,
+                    "Southern\nAfrica Tropical Forest" = 1,
+                 "Southern\nAfrica Savanna/grassland" = 3,
+                 "Southern\nAfrica Desert" = 5,
+                 "Northern\nAfrica Tropical Forests" = 1,
+                 "Northern\nAfrica Savanna/grassland" = 3,
+                 "Northern\nAfrica Desert" = 5,
+                 "Indo-Malay\nTropical Forest" = 1,
+                 "Indo-Malay\nDry tropical forest" = 2,
+                 "Southern\nAmerica Tropical Forest" = 1,
+                 "Southern\nAmerica Savanna/grassland" =3,
+                 "Southern\nAmerica Desert" = 5,
+                 "Northern\nAmerica Tropical forest" = 1,
+                 "Northern\nAmerica temperate woodland" = 2,
+                 "Northern\nAmerica Savanna/grassland" = 3,
+                 "Northern\nAmerica Desert" = 5,
+                 "Northern\nAmerica Boreal Forest" = 6,
+                 "Northern\nAmerica tundra" = 7,
+                 "Eurasia\nTemperate forest/woodland" = 2,
+                 "Eurasia\nparkland/grass" = 3,
+                 "Mediterranean" = 4,
+                 "Eurasia\nDesert/scrub" = 5,
+                 "Eurasian\nBoreal forest" = 6,
+                 "Eurasian\nTundra" = 7)
+
 regions = raster(regions)
