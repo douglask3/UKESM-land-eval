cc292b6fa88600349759695bc8d38f7f54d40334
Douglas Kelley <douglas.i.kelley@gmail.com>
2020-09-02 16:53:50 +0100

    tidying and doing multiple obs datasets

--- biomeCluster_eg.r
+++ biomeCluster_eg.r
@@ -2,24 +2,87 @@ library(raster)
 graphics.off()
 library(rasterPlot)
 
-herb = raster('outputs/veg_frac/VCF-herb-fracCover.nc')
-tree = raster('outputs/veg_frac/VCF-trees-fracCover.nc')
-fire = mean(brick('../LimFIRE/outputs/fire2000-2014.nc')[[1:12]])*12
-prec = mean(brick('../LimFIRE/outputs/Prc2000-2014.nc')[[1:12]])*12
+dir = "outputs/veg_frac/"
+
+obs  = c("cci", "igbp")
+vars = c("herb", "trees") 
+
+realmNames = c("Global", "Australia", "Southern\nAfrica", "Northern\nAfrica", "SE\nAsia",
+               "Southern\nAmerica", "Temperate\nAmerica", "Boreal\nAmerica",
+               "Temperate\nEurasia", "Boreal\nEurasia")
+
 cols = c('#ffffe5','#f7fcb9','#d9f0a3','#addd8e','#78c679','#41ab5d',
          '#238443','#006837','#004529')
 
 rmask = raster('../UKESM-land-evaluation/outputs/realms.nc')
 
-extent = c(25.1, 25.2, -2, 22)
+files = list.files(dir, full.names=TRUE)
+forDataset <- function(id) {
+    files = files[grepl(id, files)]
+    
+    herb = files[grepl(vars[1], files)]
+    tree = files[grepl(vars[2], files)]
+    
+    herb = raster(herb)
+    tree = raster(tree)   
+    
+    mask = !is.na(herb + tree + rmask)
+    forRegion <- function(rid, name) {    
+        if (!is.na(rid)) mask = mask & (rmask == rid)
+        hs = herb[mask]
+        ts = tree[mask]
+        bs = 1 - ts - hs
 
-herb = herb_obs = raster::resample(herb, rmask)
-tree = tree_obs = raster::resample(tree, rmask)
-fire = tree_obs = raster::resample(fire, rmask)
-prec = tree_obs = raster::resample(prec, rmask)
+        x = bs + 0.5 * ts
+        y = ts
+        if (length(x) == 0) {
+            plot(0, 0, pch = 20, cex = 2, col = cols, axes = FALSE, type = 'n',
+            xlab = '', ylab = '', xlim = c(0, 1), ylim = c(0,1))
+        } else {
+            cols = cols[unlist(mapply(rep, 1:9, 9 + (1:9)^3))]
+            cols = densCols(x,y, colramp = colorRampPalette(cols))
+            plot(y~x, pch = 20, cex = 2, col = cols, axes = FALSE,
+                xlab = '', ylab = '', xlim = c(0, 1), ylim = c(0,1))
+        }
+        lines(c(0, 1, 0.5, 0), c(0, 0, 1,0))
+        text(x = 0.5, y = -0.07, 'Bare Ground', xpd = TRUE)
+        text(x = 0.15, y = 0.54, 'Herb Cover', xpd = TRUE, srt = 60)
+        text(x = 0.85, y = 0.54, 'Wood Cover', xpd = TRUE, srt = -60)
+
+        x = seq(0, 1, 0.2)
+
+        lapply(x, function(i) {
+                lines(c(0.5*i, 1-0.5*i), c(i,i), lty = 2, col = '#00000099');
+                text(x=1-0.5*i+0.05, y=i, paste(i*100, '%'), xpd = TRUE, srt = -60)})
+
+        lapply(x, function(i) {
+                lines(c(i, 0.5+0.5*i), c( 0, 1-i), lty = 3, col = '#00000099');
+                text(x=i, y=-0.03, paste(i*100, '%'), xpd = TRUE, srt = 0)})
+
+        lapply(x, function(i) {
+                lines(c(0.5-0.5*i, 1-i), c( 1-i, 0), lty = 4, col = '#00000099');
+                text(x=0.45-0.5*i, y=1-i, paste(i*100, '%'), xpd = TRUE, srt = 60)})
+        mtext(side = 3, adj = 0.9, name, line = -2)
+    }
+    fname = paste0("figs/", id, "_vegDist.png")
+    png(fname, width = 7.2, height = 7.2 * sqrt(3) * 0.5 * 4/3, res = 300, units = 'in')
+        layout(rbind(c(1, 0, 11), 2:4, 5:7, 8:10))
+        par( mar = rep(1, 4))
+        mapply(forRegion, c(NaN, 1:9), realmNames)
+    dev.off()
+}
+lapply(obs, forDataset)
 
 png("figs/trasect_AFRICA.png", width = 5, height = 5, units = 'in',res = 300)
+
+fire = mean(brick('../LimFIRE/outputs/fire2000-2014.nc')[[1:12]])*12
+prec = mean(brick('../LimFIRE/outputs/Prc2000-2014.nc')[[1:12]])*12
 par(mfrow = c(2,1), mar = c(0.7, 3, 0.7, 0.5), oma = c(2.75, 0, 0, 0))
+
+
+extent = c(25.1, 25.2, -2, 22)
+fire = tree_obs = raster::resample(fire, rmask)
+prec = tree_obs = raster::resample(prec, rmask)
 transect <- function(name, addFire = TRUE) {
     tl = raster::crop(tree, extent)
     hl = raster::crop(herb, extent)
@@ -48,57 +111,6 @@ transect <- function(name, addFire = TRUE) {
 }
 transect('Observations')
 
-mask = !is.na(herb + tree)
-forRegion <- function(rid, name) {
-    
-    if (!is.na(rid)) mask = mask & (rmask == rid)
-    hs = herb[mask]
-    ts = tree[mask]
-    bs = 1 - ts - hs
-
-    x = bs + 0.5 * ts
-    y = ts
-    if (length(x) == 0) {
-        plot(0, 0, pch = 20, cex = 2, col = cols, axes = FALSE, type = 'n',
-        xlab = '', ylab = '', xlim = c(0, 1), ylim = c(0,1))
-    } else {
-        cols = cols[unlist(mapply(rep, 1:9, 9 + (1:9)^3))]
-        cols = densCols(x,y, colramp = colorRampPalette(cols))
-        plot(y~x, pch = 20, cex = 2, col = cols, axes = FALSE,
-            xlab = '', ylab = '', xlim = c(0, 1), ylim = c(0,1))
-    }
-    lines(c(0, 1, 0.5, 0), c(0, 0, 1,0))
-    text(x = 0.5, y = -0.07, 'Bare Ground', xpd = TRUE)
-    text(x = 0.15, y = 0.54, 'Herb Cover', xpd = TRUE, srt = 60)
-    text(x = 0.85, y = 0.54, 'Wood Cover', xpd = TRUE, srt = -60)
-
-    x = seq(0, 1, 0.2)
-
-    lapply(x, function(i) {
-            lines(c(0.5*i, 1-0.5*i), c(i,i), lty = 2, col = '#00000099');
-            text(x=1-0.5*i+0.05, y=i, paste(i*100, '%'), xpd = TRUE, srt = -60)})
-
-    lapply(x, function(i) {
-            lines(c(i, 0.5+0.5*i), c( 0, 1-i), lty = 3, col = '#00000099');
-            text(x=i, y=-0.03, paste(i*100, '%'), xpd = TRUE, srt = 0)})
-
-    lapply(x, function(i) {
-            lines(c(0.5-0.5*i, 1-i), c( 1-i, 0), lty = 4, col = '#00000099');
-            text(x=0.45-0.5*i, y=1-i, paste(i*100, '%'), xpd = TRUE, srt = 60)})
-    mtext(side = 3, adj = 0.9, name, line = -2)
-}
-
-png("figs/obs_vegDist.png", width = 7.2, height = 7.2 * sqrt(3) * 0.5 * 4/3, res = 300, units = 'in')
-layout(rbind(c(1, 0, 11), 2:4, 5:7, 8:10))
-par( mar = rep(1, 4))
-mapply(forRegion, c(NaN, 1:9, NaN), c("Global", "Australia", "Southern\nAfrica", "Northern\nAfrica", "SE\nAsia", "Southern\nAmerica", "Temperate\nAmerica", "Boreal\nAmerica", "Temperate\nEurasia", "Boreal\nEurasia", ''))
-
-text(0.85, 0.11, 'Desert', cex = 1.5, font = 2, srt = -30)
-text(0.23, 0.05, 'Grassland', cex = 1.5, font = 2, srt = 0)
-text(0.5, 0.2, 'Scrub', cex = 1.5, font = 2, srt = 0)
-text(0.23, 0.2, 'Savanna', cex = 1.5, font = 2, srt = 30)
-text(0.35, 0.6, 'Forest', cex = 1.5, font = 2, srt = 60)
-dev.off()
 
 
 tree = tree_mod = raster('../UKESM-land-evaluation/outputs/veg_frac/u-bc179-wood-fracCover.nc')
