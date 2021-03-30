f9f73023a9f90d20bc167140b5287f48fa686d65
Douglas Kelley <douglas.i.kelley@gmail.com>
2021-03-30 20:36:39 +0100

    setting up for GPP

--- plotSeason-LAI.r
+++ plotSeason-LAI.r
@@ -22,27 +22,33 @@ graphics.off()
 
 global_extent = extent(c(-180, 180, -60, 90))
 
-sim_files = list.files("outputs/LAI/", pattern  = "u-", full.names = TRUE)
-files = list("Observation" = 'outputs/lai_0.5x0.5_2000-2005-LAI.nc',
-             "Simulation"  = sim_files[grepl("control", sim_files)])
           
 sim_files = list.files("outputs/GPP/", pattern  = "u-", full.names = TRUE)
 files = list("Observation" = 'outputs/GPP/Observation-controlgpp_0.5x0.5.nc',
              "Simulation"  = sim_files[grepl("control", sim_files)])
           
+obsSc = 60*60*24*360
+
 axisMonth = c(2, 6, 4, 8)
 greens9   = c("#FBFFFE", "#F7FFFB", "#EAFDEF",  "#DEF7EB", "#C6EFDB", "#9EE1CA", "#6BD6AE", 
             "#42C692", "#21B571", "#089C51", "#086B30", "#043617", "#001108")
 
-units_aa = '' 
+aa_cols = c('#ffffcc','#999996','#2c7fb8','#202030','#101020', '#000020')
 
-modMapping = list(7:12, 1:6)               
+limits_aa = c(0, 0.1, 0.2, 0.5, 1, 2, 3, 4)
+dlims_aa = c(-2, -1, -0.5, -0.2, -0.1, 0.1, 0.2, 0.5, 1, 2)
+dcols_aa = c('#7f3b08','#b35806','#e08214','#fdb863','#fee0b6','#f7f7f7','#d8daeb','#b2abd2','#8073ac','#542788','#2d004b')
+
+aa_title = "Simulated veg distribution"
+units_aa = 'kg~m-2~ ~yr-1~'
+
+modMapping = NULL#list(7:12, 1:6)               
 
 run <- function(name) {    
     dlapply <- function(r,  ...) lapply(r, lapply, ...)
     dsapply <- function(r,  ...) sapply(r, sapply, ...)
     obs = dlapply(files,  brick)
-    obs[[1]][[1]]= obs[[1]][[1]]*60*60*24*1000
+    obs[[1]][[1]]= obs[[1]][[1]] * obsSc
     mxLayers = min(unlist(dsapply(obs, nlayers)))
     obs = obs_ia = dlapply(obs, function(i) i[[1:mxLayers]])
     
@@ -58,19 +64,16 @@ run <- function(name) {
         }
         return(r)
     }
-    obs = list(lapply(obs[[1]], switchMonths), lapply(obs[[2]], switchMonths, modMapping))   
+    #obs = list(lapply(obs[[1]], switchMonths), lapply(obs[[2]], switchMonths, modMapping))   
     
     #obs_in = obs
     #obs[[1]] = obs_in[[1]][[c(12, 1:11)]]
     #obs[[2]] = obs_in[[2]][[c(12, 1:11)]]
     #obs[[2]] = obs_in[[2]][[c(7:12, 1:6)]]
     
-    fname = paste0("figs/fire_var_seasonality-maps-AA", name, ".png")
-    png(fname, height = 4, width = 7.2, res = 300, units = 'in')
+    fname = paste0("figs/fire_var_seasonality-maps-AA-scatter", name, ".png")
+    png(fname, height = 4, width = 7.2/2, res = 300, units = 'in')
         par(oma = c(1, 2, 1, 0))
-        #layout(cbind(c(1, 1, 6, 6, 7, 7), c(2, 3, 8, 8, 11, 11), c(2, 3, 8, 9, 9, 11), c(2, 3, 8, 8, 11, 11), c(4, 5, 10, 10, 12, 12)),
-        #       heights = c(1, 1, 0.6, 0.4, 0.65, 0.35), width = c(1, 0.55, 0.3, 0.15, 1))
-        layout(cbind(c(1, 1), c(2, 3)), widths = c(1, 1), heights = c(1, 1))
         
         mask = !any(is.na(obs[[1]][[1]]+obs[[2]][[1]]))
         maskFUN <- function(r) lapply(r, function(i) as.vector(mean(i)[mask]))
@@ -88,7 +91,7 @@ run <- function(name) {
         plot(yp~xp, pch = 19, col = cols, cex = 1, xaxs = 'i', yaxs = 'i',
              xlab = '', ylab = '', xaxt = 'n', yaxt = 'n')
         
-        addLetLab('a')
+        #addLetLab('a')
         mtext.units(side = 1, line = 2, names(files)[1])
         mtext.units(side = 2, line = 2, names(files)[2])
         addAxis <- function(labels, side) {
@@ -103,12 +106,22 @@ run <- function(name) {
         #abline(lm(y~x))
         mtext.units(side = 3, paste0("~R2~: ",  round(cor(x, y)^2, 2)), line = -2.8, adj = 0.1)
         mtext.units(side = 3, "p < 0.001", line = -4, adj = 0.1)
+    dev.off()
+    fname = paste0("figs/fire_var_seasonality-maps-AA-maps", name, ".png")
+    png(fname, height = 5.6, width = 4, res = 300, units = 'in')
+        par(oma = c(1, 3, 3, 0), mfrow = c(3, 1), mar = rep(0, 4)) 
         
-        par(mar = rep(0, 4)) 
-        
-        aas = mapply(plotAA, obs, c('b', 'c'), names(files),
+        aas = mapply(plotAA, obs[2:1], '', c('UKESM', 'Observed'),
                      units = units_aa,addLegend = c(FALSE, TRUE) )
-        
+        diff = layer.apply(obs[[2]], function(i) i- obs[[1]][[1]])
+       
+        plotStandardMap(mean(diff), e = eFromRange(diff),
+                        limits = dlims_aa, 
+                        cols = dcols_aa, units = units_aa)
+        mtext("Difference", side = 2, adj = 0.9, line = -0.2)
+        StandardLegend(mean(diff), limits = dlims_aa, cols = dcols_aa, units = units_aa,
+                                  add = TRUE, oneSideLabels = FALSE, extend_min = TRUE)
+        mtext(side = 3, outer = TRUE, aa_title)
         scoresRegion <- function(region, r, regions) {
             scoreEns <- function(sim, obs) {
                 if (!is.nan(region)) {
@@ -133,19 +146,25 @@ run <- function(name) {
         ##srank.annual = srank.raster(obs_ia[[1]], obs_ia[[2]], 'j', 'monthly rank')
     fname = paste0("figs/fire_var_seasonality-maps-MPC", name, ".png")
     png(fname, height = 3.5, width = 7.2, res = 300, units = 'in')
+        
         par(oma = c(1, 2, 1, 0), mar = rep(0, 4))
         layout(cbind(c(1, 2, 3), c(4, 6, 7), c(5, 8, 0)))
 
-        modals = mapply(ModalMap, obs, names(files), c(F, T), let = c('d', 'g')) 
+        modals = mapply(ModalMap, obs, c('', ''), c(F, T), let = c('Modality', '')) 
+    
+        mtext(side = 2, 'Observation', adj = 1-1/8, outer = TRUE)
+        mtext(side = 2, 'Simulation', adj = 0.5, outer = TRUE)
+        mtext(side = 2, '(min)', adj = 0.45, outer = TRUE, line = -1)
+        mtext(side = 2, '(max)', adj = 1/8, outer = TRUE, line = -1)
         
-        mapply(plotConPhase, obs, list(c('e', 'f'), c('h', 'i')), c(F, T))  
+        mapply(plotConPhase, obs, list(c('Phase', 'Concentration'), c('', '')), c(F, T))  
     dev.off()    
         
     plotRegion <- function(region, name, axisMonth) {
         
         tempFile = paste0("temp/polarClim_for_region-3ID-", region, 
                           tail(strsplit(files[[2]][1], '-')[[1]], 2)[1], ".Rd")
-        if (file.exists(tempFile)) {
+        if (file.exists(tempFile) && F) {
             load(tempFile)
         } else {
             print(region)
@@ -170,7 +189,7 @@ run <- function(name) {
         }
         xlim =  c(-1,1)
         
-        
+       
         polarPlot.setup(1:12, obsv[[1]][[1]][2,],
                         type = 'l', xlim = xlim, col = 'blue', lwd = 2)
         lapply(obsv[[1]], function(i) polarPlot.lines(1:12, i[2,], col = 'blue', lwd = 2))
@@ -204,13 +223,29 @@ run <- function(name) {
     dev.off()
 }
 
-run("control")
+run("control-gpp")
 
-files[2] = "outputs/u-az513-trees-LAI.nc"
-run("trees")
+#files[2] = sim_files[grepl("control", sim_files)
+#run("trees")
+aa_title = "Corrected veg distribution"
+files[2] = sim_files[grepl("-obsVegDist", sim_files) & !grepl("trees", sim_files)]
+run("obsVegDist-gpp")
 
-files[2] = "outputs/u-az513-obsVegDist-LAI.nc"
-run("obsVegDist")
+aa_cols = c('#f7fcfd','#e5f5f9','#ccece6','#99d8c9','#66c2a4','#41ae76','#238b45','#006d2c','#00441b')
+limits_aa = c(0, 0.1, 0.2, 0.5, 1, 2, 3, 4)
+dlims_aa = c(-2, -1, -0.5, -0.2, -0.1, 0.1, 0.2, 0.5, 1, 2)
+dcols_aa = c('#8c510a','#bf812d','#dfc27d','#f6e8c3','#f5f5f5','#c7eae5','#80cdc1','#35978f','#01665e')
+units_aa = '' 
 
-files[2] = "outputs/u-az513-trees-obsVegDist-LAI.nc"
-run("obsVegDist-trees")
+
+sim_files = list.files("outputs/LAI/", pattern  = "u-", full.names = TRUE)
+files = list("Observation" = 'outputs/lai_0.5x0.5_2000-2005-LAI.nc',
+             "Simulation"  = sim_files[grepl("control", sim_files)])
+
+obsSc = 1
+run("control-lai")
+
+files[2] = sim_files[grepl("-obsVegDist", sim_files) & !grepl("trees", sim_files)]
+run("obsVegDist-lai")
+#files[2] = sim_files[grepl("control", sim_files)
+#run("obsVegDist-trees")
